1d5931e5d1ca9f68222fabb7264432315b0b687b
Frank Pswarai <frank.pswarai@students.plymouth.ac.uk>
2024-10-07 18:43:36 +0100

    Finished Project

--- MASM/main.asm
+++ MASM/main.asm
@@ -7,15 +7,15 @@ ExitProcess PROTO, dwExitCode: DWORD  ; Exit process prototype
 .data ; data segment
 	; define your variables here
 	final_result DWORD ?
-	x_1 DWORD 35
-	y_1 DWORD 45
-	c_1 DWORD 60
-	d_1 DWORD 55
-	contribution DWORD 50
-	first_numerator DWORD ?
-	second_numerator DWORD ?
-	whole_numerator DWORD ?
-	denominator DWORD ?
+	x_1 DWORD 35                     ;this is the variable for the value x
+	y_1 DWORD 45                     ;this is the variable for the value y
+	c_1 DWORD 60                     ;this is the variable for the value c
+	d_1 DWORD 55                     ;this is the variable for the value d
+	contribution DWORD 50            ;this is the individual contribution towards the coursework
+	first_numerator DWORD ?          ;this is the result of multiplying 50*x*d
+	second_numerator DWORD ?         ;this is the result of multiplying 50*y*c
+	whole_numerator DWORD ?          ;this is the total from adding the two numerators, hence the whole numerator
+	denominator DWORD ?              ;this is the denominator as a result of multiply c*d
 
 .code ; code segment
 
@@ -23,27 +23,27 @@ main PROC ; main procedure
 	; write your assembly code here
 	mov eax, x_1                ;move the value of x into the register eax
 	mul d_1                     ;multiply x by d and store the value in register eax
-	mul contribution            ;multiply the current eax value by the contribution
+	mul contribution            ;multiply the x*d value by 50 (the contribution)
 	mov first_numerator, eax    ;move the result into the variable first_numerator
 
-	mov eax, contribution
-	mul y_1
-	mul c_1
-	mov second_numerator, eax
+	mov eax, contribution       ;set eax as 50 (the contribution)
+	mul y_1                     ;multiply 50 by y
+	mul c_1                     ;multiply 50*y by c
+	mov second_numerator, eax   ;move the result into the variable second_numerator
 
-	mov ebx, first_numerator
-	mov ecx, second_numerator
+	mov ebx, first_numerator    ;move the first numerator into the register ebx
+	mov ecx, second_numerator   ;move the second numerator into the register ecx
 
-	add ebx, ecx
+	add ebx, ecx                ;add the registers ebx + ecx in order to find out what the whole value of the numerator is
 	mov whole_numerator, ebx
 
-	mov eax, c_1
-	mul d_1
-	mov denominator, eax
+	mov eax, c_1                ;set the value of eax to c
+	mul d_1                     ;multiply c * d to find out the the value of the denominator
+	mov denominator, eax        
 
-	mov eax, whole_numerator
-	div denominator
-	mov final_result, eax
+	mov eax, whole_numerator    ;in order to perform division, we must set teh value of eax to that of the divident, that is, the whole numerator
+	div denominator             ;divide the whole numerator (eax) by the denominator, to find the final result
+	mov final_result, eax       ;set teh final result variable from eax. This should display the final result as 70, that is 70%
 
 	INVOKE ExitProcess, 0 ; call exit function
   
